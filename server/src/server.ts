ddbfd51e0474ddc1e920c057eb66e98f32259da4
James Cuénod <4253884+jcuenod@users.noreply.github.com>
2025-07-01 20:42:37 -0500

    Update tmLanguage + compiler + first pass at providing type hints

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,10 @@ import {
 } from "vscode-languageserver/node";
 import { TextDocument } from "vscode-languageserver-textdocument";
 import compile from "@chicory-lang/compiler"
+import {TypeHint} from "@chicory-lang/compiler/build/env"
+
+let typeHints: TypeHint[] = []
+
 
 const connection = createConnection(ProposedFeatures.all);
 
@@ -18,6 +22,7 @@ connection.onInitialize((params: InitializeParams) => {
   const result: InitializeResult = {
     capabilities: {
       textDocumentSync: TextDocumentSyncKind.Incremental,
+      hoverProvider: true
     },
   };
 
@@ -30,19 +35,49 @@ documents.onDidChangeContent((change) => {
   // );
   try {
       const text = change.document.getText();
-      console.warn(change.document.uri)
-      const { errors } = compile(text);
-      console.warn("Compile returned errors:", JSON.stringify(errors, null, 2));
+      console.log("Document changed:", change.document.uri)
+      const { errors, hints } = compile(text);
       connection.sendDiagnostics({
         uri: change.document.uri,
         diagnostics: errors as Diagnostic[]
       })
-      console.warn("Diagnostics sent successfully");
+      typeHints = hints;
     }
     catch (e) {
       console.error("Validation error:")
       console.error(e)
     }
+});
+
+
+connection.onHover((params) => {
+  const document = documents.get(params.textDocument.uri);
+  if (!document) {
+    return null;
+  }
+  const position = params.position;
+  const offset = document.offsetAt(position);
+  
+  console.log(offset)
+
+  const validHints = typeHints.filter((hint) => {
+    const start = document.offsetAt(hint.range.start);
+    const end = document.offsetAt(hint.range.end);
+    return offset >= start && offset <= end;
+  });
+  const hintsByLength = validHints.sort((a, b) => 
+    (document.offsetAt(b.range.end) - document.offsetAt( b.range.start)) -
+    (document.offsetAt(a.range.end) - document.offsetAt( a.range.start)) 
+);
+  console.error(hintsByLength)
+  const hint = hintsByLength?.[0]
+  
+  if (hint) {
+    return {
+      contents: hint.type,
+    };
+  }
+  return null;
 });
 
 documents.listen(connection);
